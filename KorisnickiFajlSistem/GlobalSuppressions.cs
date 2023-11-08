ac018fd806bec7c4d291dd946f559a094dcc2fc7
NDj01 <nemanjadj102@gmail.com>
2023-11-08 18:08:36 +0100

    Prosirenje i sredjivanje GUI-ja

--- KorisnickiFajlSistem/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:KorisnickiFajlSistem.UserspaceFileSistem.GetFileSecurity(System.String,System.Security.AccessControl.FileSystemSecurity@,System.Security.AccessControl.AccessControlSections,DokanNet.IDokanFileInfo)~DokanNet.NtStatus")]
