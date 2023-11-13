76a9e30ea8ae21796fd2d6936d341b524b15d958
NDj01 <nemanjadj102@gmail.com>
2023-11-13 16:46:27 +0100

    sitne prepravke

--- GUI/obj/Debug/net6.0-windows/App.g.i.cs
+++ GUI/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace GUI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/GUI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GUI;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- GUI/obj/Debug/net6.0-windows/StartupWindow.g.i.cs
+++ GUI/obj/Debug/net6.0-windows/StartupWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace GUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GUI;V1.0.0.0;component/startupwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GUI;component/startupwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\StartupWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
